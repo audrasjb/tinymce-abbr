eb5ac4a26ec3f014933b6ee5c48434b4d22e04bf
Jb Audras <jeanbaptisteaudras@atelier-111.fr>
2016-09-24 15:53:41 +0200

    Backup 24/09. Note : tinymce_abbr_insertion() function is not working (see console)

--- admin/js/tinymce-abbr-class.js
+++ admin/js/tinymce-abbr-class.js
@@ -4,8 +4,60 @@
 		editor.addButton('tinymce_abbr_class', {
 			title: 'Insert abbreviation',
 			image: url + '/images/abbr-icon.png',
-			cmd: 'tinymce_abbr_class',
+			cmd: 'tinymce_abbr_modal',
 		});		
+
+		// Called when we click the Insert Gistpen button
+		editor.addCommand( 'tinymce_abbr_modal', function() {
+			// Check we have selected some text that we want to link
+			var text = editor.selection.getContent({
+				'format': 'html'
+			});
+			if ( text.length === 0 ) {
+				alert( 'Please select some text first.' );
+				return;
+			}
+			// Calls the pop-up modal
+			editor.windowManager.open(
+				{
+					// Modal settings
+					title: 'Insert abbreviation',
+//					file:  url + '/tinymce-abbr-dialog.html',
+					id: 'tinymce-abbr-insert-dialog',
+					buttons: 
+					[
+						{
+                   			type   : 'textbox',
+				   			name   : 'abbrTitle',
+				   			label  : 'Title',
+				   			tooltip: 'The meaning of your abbreviation',
+               			},
+               			{
+							text: 'Insert',
+							id: 'tinymce-abbr-button-insert',
+							class: 'insert',
+							onclick: function( editor, url ) {
+								tinymce_abbr_insertion(abbrTitle);
+							},
+						},
+					],
+                },
+                {
+                	editor: editor,                   //    This is a reference to the current editor. We'll need this to insert the shortcode we create.
+				}				
+			);
+
+		});
+
+	});
+
+	function tinymce_abbr_insertion(abbrTitle) {
+		var title = abbrTitle;
+		editor.execCommand('mceReplaceContent', false, '<abbr class="abbr" title="' + title + '">' + text + '</a>');
+	}
+
+})();
+/*
 		// Add Command when Button Clicked
 		editor.addCommand('tinymce_abbr_class', function() {
 			// Check we have selected some text that we want to link
@@ -29,8 +81,8 @@
 			}	
 
 			// Insert selected text back into editor, wrapping it in an anchor tag
-			editor.execCommand('mceReplaceContent', false, '<abbr class="abbr" title="' + result + '">' + text + '</abbr>');
+			editor.execCommand('mceReplaceContent', false, '<abbr class="abbr" title="' + result + '">' + text + '</a>');
 
 		});		
 	});
-})();
+})();*/
