607dfa207ba54d1ec1a21044ea7eaa787e11d6c2
Jb Audras <jeanbaptisteaudras@atelier-111.fr>
2016-09-26 21:13:05 +0200

    provides french translation for JS files

---
+++ admin/js/langs/fr.js
@@ -0,0 +1,6 @@
+tinymce.addI18n('fr',{
+"Insert abbreviation": "Insérer une abréviation",
+"The meaning of your abbreviation": "Intitulé complet de votre abréviation",
+"Please select some text first.": "Veuillez sélectionner un texte d'abord.",
+"Delete abbreviation": "Supprimer une abréviation",
+});
