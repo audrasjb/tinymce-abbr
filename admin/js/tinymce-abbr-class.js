2a456b33c330af2d54c23b08054314661d243c92
audrasjb <[audrasjb@gmail.com]>
2016-10-08 09:38:33 +0200

    first version of the single ABR button

--- admin/js/tinymce-abbr-class.js
+++ admin/js/tinymce-abbr-class.js
@@ -28,35 +28,44 @@
 				alert( 'Please select some text first.' );
 				return;
 			}
-			// Calls the pop-up modal dialog
-			editor.windowManager.open({
-				// Modal settings
-				title: 'Insert abbreviation',
-				id: 'tinymce-abbr-insert-dialog',
-				body: [
-					{
-                   		type   : 'textbox',
-						id: 'tinymce-abbr-title',
-				   		name   : 'abbrTitle',
-				   		label  : 'Title',
-				   		tooltip: 'The meaning of your abbreviation',
-               		},
-					{
-                   		type   : 'textbox',
-						id: 'tinymce-abbr-lang',
-				   		name   : 'abbrLang',
-				   		label  : 'Language (optional)',
-				   		tooltip: 'Example: fr, en, de, etc. Use it only if the abbreviation’s language is different from page main language',
-               		},
-			   	],
-			   	onsubmit: function(e) {
-					var text = editor.selection.getContent({
-						'format': 'html'
-					});
-					editor.execCommand('mceReplaceContent', false, '<abbr class="abbr" title="' + jQuery('#tinymce-abbr-title').val() + '" lang="' + jQuery('#tinymce-abbr-lang').val() + '">' + text + '</abbr>');
-					editor.windowManager.close();
-				}
-			});
+
+			// Check current editor selection and fire the good behavior based on the node selected
+			var node = editor.selection.getNode();
+			if (node.nodeName == 'ABBR') {
+				// If ABBR is already present then remove it
+				var newTxt = document.createTextNode(text);
+				node.parentNode.replaceChild(newTxt, node);
+			} else {
+				// If this node is not an abbr, then call abbreviation modal dialog
+				editor.windowManager.open({
+					// Modal settings
+					title: 'Insert abbreviation',
+					id: 'tinymce-abbr-insert-dialog',
+					body: [
+						{
+                   			type   : 'textbox',
+				   			id: 'tinymce-abbr-title',
+				   			name   : 'abbrTitle',
+				   			label  : 'Title',
+				   			tooltip: 'The meaning of your abbreviation',
+               			},
+			   			{
+                   			type   : 'textbox',
+				   			id: 'tinymce-abbr-lang',
+				   			name   : 'abbrLang',
+				   			label  : 'Language (optional)',
+				   			tooltip: 'Example: fr, en, de, etc. Use it only if the abbreviation’s language is different from page main language',
+               			},
+			   		],
+			   		onsubmit: function(e) {
+						var text = editor.selection.getContent({
+							'format': 'html'
+						});
+						editor.execCommand('mceReplaceContent', false, '<abbr class="abbr" title="' + jQuery('#tinymce-abbr-title').val() + '" lang="' + jQuery('#tinymce-abbr-lang').val() + '">' + text + '</abbr>');
+						editor.windowManager.close();
+					}
+				});
+			}
 		});
 	});
 })();
